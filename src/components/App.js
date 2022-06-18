1559d80a076f0f2f24cf397810c1b73b1ba069f8
judinilson monchacha <judinilson0000@gmail.com>
2022-06-18 16:25:59 +0400

    create post and list it

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
-import logo from "../logo.png";
 import Web3 from "web3";
-import Navbar from "./NavBar";
+//import Identicon from "identicon.js";
 import "./App.css";
+import SocialNetwork from "../abis/SocialNetwork.json";
+import Navbar from "./NavBar";
+import Main from "./Main";
 
 class App extends Component {
   async componentWillMount() {
@@ -29,8 +31,53 @@ class App extends Component {
     const accounts = await web3.eth.getAccounts();
     this.setState({ account: accounts[0] });
     // Network ID
-    //const networkId = await window.web3.eth.net.getId();
-    //const networkData = SocialNetwork.networks[networkId];
+    const networkId = await web3.eth.net.getId();
+    const networkData = SocialNetwork.networks[networkId];
+
+    if (networkData) {
+      const socialNetwork = web3.eth.Contract(
+        SocialNetwork.abi,
+        networkData.address
+      );
+      this.setState({ socialNetwork });
+      const postCount = await socialNetwork.methods.postCount().call();
+      this.setState({ postCount });
+      // Load Posts
+      for (var i = 1; i <= postCount; i++) {
+        const post = await socialNetwork.methods.posts(i).call();
+        this.setState({
+          posts: [...this.state.posts, post],
+        });
+      }
+
+      // Sort posts. Show highest tipped posts first
+      this.setState({
+        posts: this.state.posts.sort((a, b) => b.tipAmount - a.tipAmount),
+      });
+      this.setState({ loading: false });
+    } else {
+      window.alert("SocialNetwork contract not deployed to detected network.");
+    }
+  }
+
+  createPost(content) {
+    this.setState({ loading: true });
+    this.state.socialNetwork.methods
+      .createPost(content)
+      .send({ from: this.state.account })
+      .once("receipt", (receipt) => {
+        this.setState({ loading: false });
+      });
+  }
+
+  tipPost(id, tipAmount) {
+    this.setState({ loading: true });
+    this.state.socialNetwork.methods
+      .tipPost(id)
+      .send({ from: this.state.account, value: tipAmount })
+      .once("receipt", (receipt) => {
+        this.setState({ loading: false });
+      });
   }
   constructor(props) {
     super(props);
@@ -42,15 +89,25 @@ class App extends Component {
       loading: true,
     };
 
-    //this.createPost = this.createPost.bind(this);
-    //this.tipPost = this.tipPost.bind(this);
+    this.createPost = this.createPost.bind(this);
+    this.tipPost = this.tipPost.bind(this);
   }
 
   render() {
     return (
       <div>
-        {" "}
         <Navbar account={this.state.account} />
+        {this.state.loading ? (
+          <div id="loader" className="text-center mt-5">
+            <p>Loading...</p>
+          </div>
+        ) : (
+          <Main
+            posts={this.state.posts}
+            createPost={this.createPost}
+            tipPost={this.tipPost}
+          />
+        )}
       </div>
     );
   }
